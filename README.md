023468f12c5c8179e949d295e55f8018ef247a84
Wellington Andrade <wellington021.andrade@gmail.com>
2023-08-19 14:50:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bootcamp Santander - Flutter
+ Primeiro app abordado no programa de Bootcamps da Santander em parceria com a DIO
---
+++ android/app/src/main/kotlin/com/wellingtonzero21/dioapp/myapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wellingtonzero21.dioapp.myapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
