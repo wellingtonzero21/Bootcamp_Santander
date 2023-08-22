b5519f2366f3ce424ba3d4cc32cd55ef99bf9cb0
Wellington Andrade <wellington021.andrade@gmail.com>
2023-08-22 17:13:31 -0300

    loops

---
+++ fundamentos_dart/lib/loops/for.dart
@@ -0,0 +1,31 @@
+void main(List<String> args) {
+  for(var i = 0; i <= 11; ++i) {
+    print(i);
+  }
+
+  var texto = "Bootcamp DIO";
+  for (var i = 0; i < texto.length; i++) {
+    print(texto[i]);
+  }
+
+  var listaMomes = ["Danilo","Roberto Carlos","Vanderlei",];
+  for (var i = 0; i < listaMomes.length; i++) {
+    print(listaMomes[i]);
+  }
+
+  var numeros = [1, 3, 6 , 9, 12];
+  var acumulador = 0;
+  for (var numero in numeros) {
+    acumulador = acumulador + numero;
+    print(acumulador);
+  }
+
+  var letras = ["D", "I", "O"];
+  for (var letras in letras) {
+    print(letras);
+  }
+/// ou ///
+  letras.forEach((element) {
+    print(element);
+  });
+}
---
+++ fundamentos_dart/lib/loops/tipos/calculadora.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  print("Bem vindo a calculadora :D");
+  print("Digite um numero: ");
+  var linha = stdin.readLineSync();
+  var numero = double.parse(linha ?? "0");
+
+  print("Digite outro numero: ");
+  var linha2 = stdin.readLineSync();
+  var numero2 = double.parse(linha2 ?? "0");
+
+  print("Digite um operador matemático: (+, -, *, /)");
+  var operador = stdin.readLineSync();
+
+  ///////// if/ else if/ else \\\\\\\\\\\
+  
+  /*if(operador == "+" ) {
+    var soma = numero + numero2;
+    print("a soma entre $numero e $numero2 é: $soma");
+  } else if(operador == "-" ) {
+      var subtracao = numero - numero2;
+      print("A subtração entre $numero e $numero2 é: $subtracao");
+  } else if(operador == "*" ) {
+      var vezes = numero * numero2;
+      print("A multiplicação entre $numero e $numero2 é: $vezes");
+  } else if(operador == "/" ) {
+      var divisao = numero / numero2;
+      print("A divisão entre o numero $numero e $numero2 é: $divisao");
+  } else {print("Não entendemos seu operador"); exit(0);}*/
+
+  ///////// Switch \\\\\\\\\\\
+  
+  double resultado = 0;
+
+  switch(operador){
+    case "+":
+      resultado = numero + numero2;
+      break;
+    case "-":
+      resultado = numero - numero2;
+      break;
+    case "*":
+      resultado = numero * numero2;
+      break;
+    case "/":
+      resultado = numero / numero2;
+      break;
+      default: print("Operação inválida");
+
+  }
+  print("O resultado da operação é: $resultado");
+}
