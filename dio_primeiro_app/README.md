fa939ca18ae84b153063333d9ad9850617bd8201
Wellington Andrade <wellington021.andrade@gmail.com>
2023-09-01 16:48:21 -0300

    criando o primeiro app

--- dio_primeiro_app/README.md
+++
@@ -1,2 +0,0 @@
-# Bootcamp Santander - Flutter
- Primeiro app abordado no programa de Bootcamps da Santander em parceria com a DIO
--- dio_primeiro_app/android/app/src/main/kotlin/com/wellingtonzero21/dioapp/myapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.wellingtonzero21.dioapp.myapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
