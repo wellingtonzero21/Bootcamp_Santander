cd0f360d22fd4498df2a5bc028311a263dcd0d97
Wellington Andrade <wellington021.andrade@gmail.com>
2023-09-01 14:59:24 -0300

    Update acumulador.dart

--- fundamentos_dart/lib/functions/acumulador.dart
+++ fundamentos_dart/lib/functions/acumulador.dart
@@ -2,7 +2,6 @@ import 'package:fundamentos_dart/functions/utils.dart' as utils;
 
 void main(List<String> args) {
   var linha = utils.lerConsole("Digite um numero ou S para sair: ");
-  double acumulador = 0;
   List<double> numeros = [];
   while(linha != "S") {
     numeros.add(double.parse(linha));
