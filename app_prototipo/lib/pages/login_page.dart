f4e5d886afc59e53b11dd4e8e8853b59462457a8
Wellington Andrade <wellington021.andrade@gmail.com>
2023-09-05 18:45:57 -0300

    add app protótipo

--- app_prototipo/lib/pages/login_page.dart
+++ app_prototipo/lib/pages/login_page.dart
@@ -12,7 +12,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Color.fromARGB(255, 23, 24, 31),
+        backgroundColor: const Color.fromARGB(255, 235, 186, 137),
         body: SizedBox(
           width: double.infinity,
           child: Column(
@@ -21,25 +21,9 @@ class _LoginPageState extends State<LoginPage> {
               const SizedBox(
                 height: 100,
               ),
-              Row(
-                children: [
-                  Expanded(child: Container()),
-                  Expanded(
-                    flex: 6,
-                    child: Image.asset('assets/image/logo_dio.png') , 
-                  ),
-                  Expanded(child: Container()),
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Text("Já tem cadastro?", style: TextStyle(fontSize: 35, color: Colors.white),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              const Text("Faça seu login e make the change_", style: TextStyle(fontSize: 20, color: Colors.white),
+              const Icon(
+                Icons.person,
+                size: 170,
               ),
               Container(
                 margin: const EdgeInsets.only(top: 30, left: 40, right: 40),
@@ -65,14 +49,12 @@ class _LoginPageState extends State<LoginPage> {
                 color: const Color.fromARGB(255, 42, 168, 77),
                 child: const Text("Login"),
               ),
-              Expanded(child: Container()),
-              const Text("Esqueci minha senha", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 231, 201, 109)),),
               Container(
-                margin: const EdgeInsets.only(top: 10),
+                margin: const EdgeInsets.only(top: 10, left: 40, right: 40),
+                width: double.infinity,
                 alignment: Alignment.center,
-                child: const Text("Criar conta", style: TextStyle(fontSize: 18, color: Colors.green),),
+                child: const Text("Cadastre-se"),
               ),
-              const SizedBox(height: 70,)
             ],
           ),
         ),
