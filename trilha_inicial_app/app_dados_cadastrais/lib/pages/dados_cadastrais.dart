151e2c105c0fd055091cb7d3e86cbda5f7cd05d5
Wellington Andrade <wellington021.andrade@gmail.com>
2023-09-11 18:39:19 -0300

    add repositorios

--- trilha_inicial_app/app_dados_cadastrais/lib/pages/dados_cadastrais.dart
+++ trilha_inicial_app/app_dados_cadastrais/lib/pages/dados_cadastrais.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:primeiro_app/repositories/linguagens_repository.dart';
+import 'package:primeiro_app/repositories/nivel_repository.dart';
 
 class DadosCadastrais extends StatefulWidget {
   const DadosCadastrais({super.key});
@@ -8,32 +10,138 @@ class DadosCadastrais extends StatefulWidget {
 }
 
 class _DadosCadastraisState extends State<DadosCadastrais> {
+  var nomeController = TextEditingController(text: "");
+  var dataNascimentoController = TextEditingController(text: "");
+  DateTime? dataNascimento;
+  var nivelRepository = NivelRepository();
+  var niveis = [];
+  var nivelSelecionado = "";
+  var linguagens = [];
+  var linguagensSelecionadas = [];
+  var linguagensRepository = LinguagensRepository();
+
+  @override
+  void initState() {
+    niveis = nivelRepository.retornaNiveis();
+    linguagens = linguagensRepository.retornaLinguagem();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    var nomeController = TextEditingController(text: "");
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Meus dados", style: TextStyle(fontSize: 22,),),
+        title: const Text(
+          "Meus dados",
+          style: TextStyle(
+            fontSize: 22,
+          ),
+        ),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        padding: const EdgeInsets.only(left: 20, top: 30, right: 20),
+        child: ListView(
           children: [
             const Text(
-              "Nome",
-              style: TextStyle(fontSize: 22),
+              "Nome:",
+              style: TextStyle(fontSize: 19),
             ),
-            TextField(
-              controller: nomeController,
+            SizedBox(
+              height: 30,
+              child: TextField(
+                controller: nomeController,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Data de Nascimento:",
+                      style: TextStyle(fontSize: 19),
+                    ),
+                  ]),
+            ),
+            SizedBox(
+              height: 30,
+              child: TextField(
+                controller: dataNascimentoController,
+                readOnly: true,
+                onTap: () async {
+                  var data = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime(2000, 01, 01),
+                      firstDate: DateTime(1900, 01, 01),
+                      lastDate: DateTime(2023, 01, 01));
+                  if (data != null) {
+                    dataNascimentoController.text = data.toString();
+                    dataNascimento = data;
+                  }
+                },
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Nível de experiência:",
+                      style: TextStyle(fontSize: 19),
+                    ),
+                  ]),
+            ),
+            Column(
+              children: niveis
+                  .map((nivel) => RadioListTile(
+                      title: Text(nivel.toString()),
+                      selected: nivelSelecionado == nivel,
+                      value: nivel.toString(),
+                      groupValue: nivelSelecionado,
+                      onChanged: (value) {
+                        setState(() {
+                          print(value);
+                          nivelSelecionado = value.toString();
+                        });
+                      }))
+                  .toList(),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Linguagem Preferidas:",
+                      style: TextStyle(fontSize: 19),
+                    ),
+                    Column(
+                      children: linguagens
+                          .map((linguagem) => CheckboxListTile(
+                              title: Text(linguagem),
+                              value: linguagensSelecionadas.contains(linguagem),
+                              onChanged: (bool? value) {
+                                  if (value!) {
+                                    linguagensSelecionadas.add(linguagem);
+                                  } else {
+                                    linguagensSelecionadas.remove(linguagem);
+                                  }
+                                  setState(() {
+                                    
+                                  });
+                              }))
+                          .toList(),
+                    )
+                  ]),
             ),
             SizedBox(
               width: double.infinity,
-              child: TextButton(onPressed: () {
-                print("Clicado");
-              },
-              child: Text("Salvart")),
+              child: TextButton(
+                  onPressed: () {
+                    print("Clicado");
+                  },
+                  child: const Text("Salvar")),
             ),
           ],
         ),
