681a47e886b43ae12476dadd71d45b1cd958e111
Wellington Andrade <wellington021.andrade@gmail.com>
2023-09-15 15:49:27 -0300

    add animação

---
+++ trilha_inicial_app/app_dados_cadastrais/lib/model/card_detail.dart
@@ -0,0 +1,8 @@
+class CardDetail {
+  int id;
+  String title;
+  String assets;
+  String text;
+
+  CardDetail(this.id, this.title, this.assets, this.text);
+}
--- trilha_inicial_app/app_dados_cadastrais/lib/pages/card_page.dart
+++ trilha_inicial_app/app_dados_cadastrais/lib/pages/card_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:primeiro_app/model/card_detail.dart';
+import 'package:primeiro_app/pages/card_detail.dart';
 
 class CArdPage extends StatefulWidget {
   const CArdPage({super.key});
@@ -8,6 +10,9 @@ class CArdPage extends StatefulWidget {
 }
 
 class _CArdPageState extends State<CArdPage> {
+  var cardDetail = CardDetail(1, "Lorem Ipsum", "assets/image/logo_dio.png",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,48 +20,65 @@ class _CArdPageState extends State<CArdPage> {
         Container(
           margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
           width: double.infinity,
-          child: Card(
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-            elevation: 5,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        "assets/image/logo_dio.png",
-                        height: 20,
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                          "Lorem Ipsum",
-                          style: TextStyle(
-                              fontSize: 25, fontWeight: FontWeight.w700),
+          child: Hero(
+            tag: cardDetail.id,
+            child: Card(
+              shape:
+                  RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              elevation: 5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          cardDetail.assets,
+                          height: 20,
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                  child: Text(
-                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                    style: TextStyle(
-                      fontSize: 18,
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Text(
+                            cardDetail.title,
+                            style: TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ],
                     ),
-                    textAlign: TextAlign.left,
                   ),
-                ),
-                Container(
-                    width: double.infinity,
-                    alignment: Alignment.center,
-                    child: TextButton(
-                        onPressed: () {}, child: const Text("Ler mais", style: TextStyle(decoration: TextDecoration.underline),)))
-              ],
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    child: Text(
+                      cardDetail.text,
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.justify,
+                    ),
+                  ),
+                  Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => CardDetailPage(
+                                    cardDetail: cardDetail,
+                                  ),
+                                ));
+                          },
+                          child: const Text(
+                            "Ler mais",
+                            style:
+                                TextStyle(decoration: TextDecoration.underline),
+                          )))
+                ],
+              ),
             ),
           ),
         ),
