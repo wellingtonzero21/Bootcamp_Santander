7c8c10cd2ac5bbdc1bf6b4c40bb74c9b2177a79a
Wellington Andrade <wellington021.andrade@gmail.com>
2024-01-16 14:22:46 -0300

    finalizando class

--- dart_fundamentos/lib/loops/class.dart
+++ dart_fundamentos/lib/loops/class.dart
@@ -1,18 +1,25 @@
 void main(List<String> args) {
-  print(celular1.nome);
-  print(celular1.processadores);
-  print(celular1.tamanho);
-  print(celular1.peso);
+  Celular celularDavid = Celular('Azul', 5, 6.7, 0.800);
+  Celular celularZe = Celular('Preto', 3, 6.7, 0.700);
+  print(celularDavid.toString());
+  print(celularZe.toString());
+  print(celularDavid.valorCelular(500));
+  print(celularZe.valorCelular(500));
 }
 
-var celular1 = Celular('Nokia', 5, 6.7, 0.800);
-
 class Celular {
-  final String nome;
+  final String cor;
   final int processadores;
   final double tamanho;
   final double peso;
 
-  Celular(this.nome, this.processadores, this.tamanho, this.peso);
-}
+  Celular(this.cor, this.processadores, this.tamanho, this.peso);
 
+  String toString() {
+    return "Cor $cor, processadores $processadores, tamanho $tamanho, peso $peso";
+  }
+
+  double valorCelular(double valor) {
+    return valor * processadores;
+  }
+}
