b7c03e06e65bb71784fd64ef14cd3303070b2856
Wellington Andrade <wellington021.andrade@gmail.com>
2024-01-22 14:46:20 -0300

    json

---
+++ dart_fundamentos/POO/json.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+void main(List<String> args) {
+  String json = '''
+  {
+    "user": "user@gmail.com",
+    "password": "123456",
+    "permissoes": [
+        "ower",
+        "admin"
+    ]
+}
+''';
+
+  print(json);
+
+  Map resultJson = jsonDecode(json);
+  print(resultJson);
+  print(resultJson.runtimeType);
+  print(resultJson["user"]);
+  print(resultJson["password"]);
+  print(resultJson["permissoes"][0]);
+
+  Map mapa = {
+    'nome' : 'David',
+    'password' : '123456',
+    'permissoes' : ["owner","admin"],
+
+  };
+  print("-------------------------");
+  print(mapa);
+
+  var result = jsonEncode(mapa);
+  print(result); 
+}
