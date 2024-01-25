db76c68fcbc368f2919dfe31aed7e5bc0a75ea1c
Wellington Andrade <wellington021.andrade@gmail.com>
2024-01-25 20:22:16 -0300

    Create callable_interface(call).dart

---
+++ dart_fundamentos/POO/callable_interface(call).dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  BuscarAlunos buscarAlunos = BuscarAlunos();
+  buscarAlunos();
+}
+
+class BuscarAlunos {
+  void call() => print("William, David, Davila"); //callable interface
+}
